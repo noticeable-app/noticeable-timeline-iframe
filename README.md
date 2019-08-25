03195d949d18d5d72e849dd074516c678b40a8d2
Laurent Pellegrino <laurent.pellegrino@gmail.com>
2019-08-25 10:11:59 +0200

    Rework iframe hash router to support Single Page App (SPA)

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # noticeable-timeline-iframe
 
-This repository shows how to embed a Noticeable Timeline in your own pages via an iframe.
+This repository shows how to embed a Noticeable Timeline in your own pages by using an iframe.
 
 ## Hash-based routing
 
@@ -10,7 +10,39 @@ In other words, the URL in the browser's address bar is not the actual URL to th
 Hash-based routing allows targeting each iframe page via a dedicated URL. 
 It is supported via a small JavaScript code snippet to include on the page that embeds the Timeline.
 
-Below is an example to open a specific page:
+Below is an example that opens a given post using hash-based routing:
 
 https://embedded.timeline.noticeable.io/changelog/#posts/improving-customer-retention
 
+### Setup
+
+Add the following code in your HTML `<head>...</head>` element:
+
+```html
+<script type="text/javascript">
+    window.noticeableSettings = {
+        iframe: {
+            // The selector to use for selecting the iframe HTML element on your page
+            selector: '#noticeable-iframe',
+            // The URL to the Noticeable Timeline served by the iframe
+            timelineUrl: 'https://timeline.noticeable.io/FAbWKLsdrqqXxOKwNAdU'
+        }
+    };
+
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://embedded.timeline.noticeable.io/noticeable-iframe-hash-router.min.js';
+    document.getElementsByTagName('head')[0].appendChild(script);
+</script>
+```
+
+and place an `iframe` element in your page content:
+
+```html
+<iframe id="noticeable-iframe" width="100%" height="100%" frameborder="0"/>
+```
+
+You might still need to style the `iframe` with CSS to make it match your page constraints or styles. For a complete 
+sample please look at the next page:
+
+https://github.com/noticeableapp/noticeable-timeline-iframe/blob/master/index.html
--- noticeable-iframe-hash-router.js
+++ noticeable-iframe-hash-router.js
@@ -1,61 +1,122 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-    var selector = window.noticeableSettings.iframe.selector;
-    var timelineUrl = window.noticeableSettings.iframe.timelineUrl;
-
-    if (!selector) {
-        console.error("Missing iframe selector definition.");
-        return;
+(function() {
+    function noticeableIframeInit() {
+        if ('complete' === document.readyState || 'interactive' === document.readyState) {
+            noticeableIframeStart();
+        } else {
+            document.addEventListener('DOMContentLoaded', function (event) {
+                noticeableIframeStart();
+            });
+        }
     }
 
-    if (!timelineUrl) {
-        console.error("Missing timeline URL definition.");
-        return;
-    }
+    function noticeableIframeStart() {
+        noticeableIframeDebug('Starting iframe handling');
 
-    var iframe = document.querySelector(selector);
-
-    if (!iframe) {
-        console.error('Invalid iframe selector:', selector);
-        return;
-    }
-
-    if (location.hash) {
-        iframe.setAttribute('src', timelineUrl + location.hash.replace('#', '/'));
-    } else {
-        iframe.src = timelineUrl;
-    }
-
-    window.addEventListener("message", function (event) {
-        try {
-            var data = JSON.parse(event.data);
-        } catch (e) {
-            // Payloads that cannot be parsed are not sent by Noticeable
+        if (!noticeableIframeCheckSettings()) {
+            return;
         }
 
-        if (data && data.type === "noticeable-timeline-location") {
-            var path = stripProjectId(data.path);
+        window.addEventListener('message', function (event) {
+            try {
+                var data = JSON.parse(event.data);
+            } catch (e) {
+                // Payloads that cannot be parsed are not sent by Noticeable
+            }
 
-            if (path) {
-                location.hash = path;
+            if (data && data.type === 'noticeable-timeline-location') {
+                var path = noticeableStripProjectId(data.path);
+
+                if (path) {
+                    location.hash = path;
+                } else {
+                    location.hash = '';
+                }
+            }
+        }, false);
+
+        var oldLocation = location.href;
+        setInterval(function () {
+            if (oldLocation && location.href !== oldLocation) {
+                noticeableIframeDebug('Location change detected', location.href);
+                noticeableIframeLoad(window.noticeableSettings.iframe.selector, window.noticeableSettings.iframe.timelineUrl);
+                oldLocation = location.href
+            }
+        }, window.noticeableSettings.iframe.pageChangeCheckInterval || 240);
+
+        noticeableIframeLoad(window.noticeableSettings.iframe.selector, window.noticeableSettings.iframe.timelineUrl);
+    }
+
+    function noticeableIframeCheckSettings() {
+        var selector = window.noticeableSettings.iframe.selector;
+        var timelineUrl = window.noticeableSettings.iframe.timelineUrl;
+
+        noticeableIframeDebug('Noticeable iframe settings', window.noticeableSettings.iframe);
+
+        if (!selector) {
+            console.error('Missing iframe selector definition.');
+            return false;
+        }
+
+        if (!timelineUrl) {
+            console.error('Missing timeline URL definition.');
+            return false;
+        }
+
+        return true;
+    }
+
+    function noticeableIframeLoad(selector, timelineUrl) {
+        var iframe = document.querySelector(selector);
+
+        if (!iframe) {
+            noticeableIframeDebug('Iframe not found on the current page with selector', selector);
+            return;
+        }
+
+        if (location.hash) {
+            var newTimelineUrl = noticeableIframeBuildUrl(timelineUrl);
+            noticeableIframeDebug('Updating timeline URL', newTimelineUrl);
+            iframe.src = newTimelineUrl;
+        } else {
+            noticeableIframeDebug('Updating timeline URL', timelineUrl);
+            iframe.src = timelineUrl;
+        }
+    }
+
+    function noticeableIframeDebug(message, object) {
+        if (window.noticeableSettings.iframe && window.noticeableSettings.iframe.debug) {
+            if (object) {
+                console.log('[DEBUG] ' + message, object);
             } else {
-                location.hash = '';
+                console.log('[DEBUG] ' + message);
             }
         }
-    }, false);
-});
-
-function stripProjectId(path) {
-    var index = 0;
-
-    if (path.indexOf('/') === 0) {
-        index = 1;
     }
 
-    index = path.indexOf('/', index);
-
-    if (index > -1) {
-        return path.substr(index + 1);
-    } else {
-        return '';
+    function noticeableIframeBuildUrl(timelineUrl) {
+        var timelineUrlChunks = timelineUrl.split('?');
+        var newTimelineUrl = timelineUrlChunks[0] + location.hash.replace('#', '/');
+        if (timelineUrlChunks.length > 1) {
+            newTimelineUrl += '?' + timelineUrlChunks[1];
+        }
+        return newTimelineUrl;
     }
-}
+
+    function noticeableStripProjectId(path) {
+        var index = 0;
+
+        if (path.indexOf('/') === 0) {
+            index = 1;
+        }
+
+        index = path.indexOf('/', index);
+
+        if (index > -1) {
+            return path.substr(index + 1);
+        } else {
+            return '';
+        }
+    }
+
+    noticeableIframeInit();
+})();
